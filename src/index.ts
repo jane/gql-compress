44e7d8baeb4602902819d270b8188bece9c153c8
zacanger <zac@zacanger.com>
2020-03-21 18:08:34 -0600

    chore: deps

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 export default (s: string = ''): string =>
   s
     // replace multiple whitespace with a single
-    .replace(/(\b|\B)\s{1,}(\b|\B)/gm, ' ')
+    .replace(/(\b|\B)\s+(\b|\B)/gm, ' ')
     // remove all whitespace between everything except for word and word boundaries
-    .replace(/(\B)\s{1,}(\B)|(\b)\s{1,}(\B)|(\B)\s{1,}(\b)/gm, '')
+    .replace(/(\B)\s+(\B)|(\b)\s+(\B)|(\B)\s+(\b)/gm, '')
     .trim()
